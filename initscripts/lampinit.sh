33392ee1f2b1740911d4a63de4ddd253b56ac80a
Jonas Stumph Stevnsvig <jonas@stevnsvig.com>
2020-12-11 18:45:24 +0100

    added mysql stuff

--- initscripts/lampinit.sh
+++ initscripts/lampinit.sh
@@ -2,15 +2,34 @@
 
 sudo sed -i 's/AcceptEnv/\#AcceptEnv/g' /etc/ssh/sshd_config
 sudo /etc/init.d/ssh reload
+
+### INSTALL APT PACKAGES ###
 sudo apt-get update
 sudo apt-get install -y apache2 php7.4-curl php7.4-gd apache2 mysql-server php7.4 unzip php7.4-mysql php-xdebug php-pear* git
 #pre-fetch modsecurity without installing it
 sudo apt-get install --download-only libapache2-mod-security2 liblua5.1-0 libyajl2 modsecurity-crs
 
-#copy xdebug config to proper location
+### APACHE ###
+# Copy xdebug config to proper location
 sudo mv /tmp/xdebug.ini /etc/php/7.4/mods-available/xdebug.ini
 
-#enable mod_ssl
-sudo a2enmod ssl
-#restarting apache2
+# Enable mod_ssl
+sudo a2enmod -q ssl
+# Restart apache2
 sudo systemctl reload apache2
+
+### MySQL ###
+# Setup mysql user
+cat <<EOD | sudo mysql -u root mysql
+  CREATE USER 'vagrant'@'%' IDENTIFIED BY 'secret';
+  GRANT ALL PRIVILEGES ON * . * TO 'vagrant'@'%';
+  FLUSH PRIVILEGES;
+EOD
+
+
+LOCAL_IP_LIST=`ip addr show | grep -Po 'inet \K[\d.]+'` 
+# Allow remote connections to mysql (don't do it like this in production)
+#sudo sed -i "s/bind-address\s*= 127.0.0.1/bind-address = $IP_ADDR,127.0.0.1/" /etc/mysql/mysql.conf.d/mysqld.cnf
+sudo ufw allow 3306
+#sudo systemctl restart mysql
+
